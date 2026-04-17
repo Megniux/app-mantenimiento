f77ff7190578af1e744939656a978edfa3c889d6
Megniux <maximiliano.freire@gmail.com>
2026-04-17 16:05:06 -0300

    Cambio de base de datos

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -3,12 +3,12 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-aut
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDew-CFyPQ8fIUPQf_vnInM9-JZEuV1zi8",
-  authDomain: "mantenimiento-app-170e5.firebaseapp.com",
-  projectId: "mantenimiento-app-170e5",
-  storageBucket: "mantenimiento-app-170e5.firebasestorage.app",
-  messagingSenderId: "555398253444",
-  appId: "1:555398253444:web:565d98dbbe52844b5bebd1"
+  apiKey: "AIzaSyC5eZDHD0Ui8n8LTlGeARnIjemKKfgBSH0",
+  authDomain: "pruebas-ae2ce.firebaseapp.com",
+  projectId: "pruebas-ae2ce",
+  storageBucket: "pruebas-ae2ce.firebasestorage.app",
+  messagingSenderId: "343975121135",
+  appId: "1:343975121135:web:a40e056e457c275b3a4b1a"
 };
 
 const app = initializeApp(firebaseConfig);
